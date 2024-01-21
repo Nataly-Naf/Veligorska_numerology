b9b0dc5aec5673343a65eb12ef66823d5ea765fc
Nataly-Naf <nat.spivak84@gmail.com>
2024-01-21 16:55:42 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: glob.sync('./src/*.html'),
     },
-    outDir: '../Veligorska_numerology',
+    outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
